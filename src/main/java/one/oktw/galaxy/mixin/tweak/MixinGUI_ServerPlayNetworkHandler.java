d17977b5d197b8aad8f4307fc63132f7a22da9fb
james58899 <james59988@gmail.com>
2019-09-29 17:26:04 +0800

    Try

--- src/main/java/one/oktw/galaxy/mixin/tweak/MixinGUI_ServerPlayNetworkHandler.java
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinGUI_ServerPlayNetworkHandler.java
@@ -18,10 +18,11 @@
 
 package one.oktw.galaxy.mixin.tweak;
 
+import net.minecraft.client.network.packet.InventoryS2CPacket;
+import net.minecraft.network.Packet;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.packet.ClickWindowC2SPacket;
-import one.oktw.galaxy.container.ContainerForceUpdate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,12 +30,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
-public class MixinGUI_ServerPlayNetworkHandler {
+public abstract class MixinGUI_ServerPlayNetworkHandler {
     @Shadow
     public ServerPlayerEntity player;
 
+    @Shadow
+    public abstract void sendPacket(Packet<?> packet_1);
+
     @Inject(method = "onClickWindow", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;sendPacket(Lnet/minecraft/network/Packet;)V"))
     private void onClickWindow(ClickWindowC2SPacket packet, CallbackInfo ci) {
-        ((ContainerForceUpdate) player.container).forceUpdateSlot(packet.getSlot());
+        sendPacket(new InventoryS2CPacket(packet.getSyncId(), player.container.getStacks()));
     }
 }
