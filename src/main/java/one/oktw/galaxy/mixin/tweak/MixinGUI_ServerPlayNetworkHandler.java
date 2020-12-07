d1f98fdadc905791e1a5efbc5a9f3735e8aaa289
james58899 <james59988@gmail.com>
2020-12-07 22:48:21 +0800

    Clean up

--- src/main/java/one/oktw/galaxy/mixin/tweak/MixinGUI_ServerPlayNetworkHandler.java
+++ src/main/java/one/oktw/galaxy/mixin/tweak/MixinGUI_ServerPlayNetworkHandler.java
@@ -18,24 +18,15 @@
 
 package one.oktw.galaxy.mixin.tweak;
 
-import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.PickFromInventoryC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class MixinGUI_ServerPlayNetworkHandler {
-    @Shadow
-    public ServerPlayerEntity player;
-
-    @Shadow
-    public abstract void sendPacket(Packet<?> packet_1);
-
     @Inject(method = "onPickFromInventory", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;sendPacket(Lnet/minecraft/network/Packet;)V"))
     private void updatedContainer(PickFromInventoryC2SPacket packet, CallbackInfo ci) {
         System.out.println("[DEBUG] Update Container");
